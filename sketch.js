366e44d2dbf771c3ed5b8c405f2c4c2fdb7d303c
kevinhb92 <leeh965@Kevinui-MacBook-Pro.local>
2015-09-11 05:37:31 -0400

    BRICKS

--- sketch.js
+++ sketch.js
@@ -7,26 +7,231 @@ function draw() {
 	// clear the background - skyblue
   background(172, 242, 240);
 
+
+  //set a fill color for the tree trunk
+	fill(58, 50, 6)
+	noStroke();
+
+   //draw tree trunk
+	 rect(155, 250, 55, 55);
+	 rect(455, 250, 55, 55);
+	 rect(355, 250, 55, 55);
+
+  //set a fiil for the tree
+	fill(36, 97, 39)
+	noStroke();
+	// draw a tree for background
+	triangle(110, 250, 185, 150, 260, 250);
+  triangle(120, 220, 185, 120, 250, 220)
+	triangle(130, 190, 185, 90, 240, 190)
+
+	triangle(410, 250, 485, 150, 560, 250);
+  triangle(420, 220, 485, 120, 550, 220)
+	triangle(430, 190, 485, 90, 540, 190)
+
+	triangle(310, 250, 385, 150, 460, 250);
+  triangle(320, 220, 385, 120, 450, 220)
+	triangle(330, 190, 385, 90, 440, 190)
+
+	triangle(110, 250, 185, 150, 260, 250);
+  triangle(120, 220, 185, 120, 250, 220)
+	triangle(130, 190, 185, 90, 240, 190)
+
+	triangle(110, 250, 185, 150, 260, 250);
+  triangle(120, 220, 185, 120, 250, 220)
+	triangle(130, 190, 185, 90, 240, 190)
+
+
 	// set a fill color for ground
 	fill(89, 63, 63);
 	// draw ground
 	noStroke();
 	rect(0, 300, 640, 100);
 
+//set a fill color for castle
+fill(196,196,179)
+//draw a castle
+rect(490, 80, 240, 240)
+rect(490, 60, 20, 20)
+rect(530, 60, 20, 20)
+rect(570, 60, 20, 20)
+rect(610, 60, 20, 20)
+
+//set a fill color for brick
+fill(101,101,92)
+//bricks
+rect(490, 80, 20, 10)
+rect(530, 80, 20, 10)
+rect(570, 80, 20, 10)
+rect(610, 80, 20, 10)
+
+rect(510, 90, 20, 10)
+rect(550, 90, 20, 10)
+rect(590, 90, 20, 10)
+rect(630, 90, 20, 10)
+
+rect(490, 100, 20, 10)
+rect(530, 100, 20, 10)
+rect(570, 100, 20, 10)
+rect(610, 100, 20, 10)
+
+rect(510, 110, 20, 10)
+rect(550, 110, 20, 10)
+rect(590, 110, 20, 10)
+rect(630, 110, 20, 10)
+
+rect(490, 120, 20, 10)
+rect(530, 120, 20, 10)
+rect(570, 120, 20, 10)
+rect(610, 120, 20, 10)
+
+rect(510, 130, 20, 10)
+rect(550, 130, 20, 10)
+rect(590, 130, 20, 10)
+rect(630, 90, 20, 10)
+
+rect(490, 140, 20, 10)
+rect(530, 140, 20, 10)
+rect(570, 140, 20, 10)
+rect(610, 140, 20, 10)
+
+rect(510, 150, 20, 10)
+rect(550, 150, 20, 10)
+rect(590, 150, 20, 10)
+rect(630, 150, 20, 10)
+
+rect(490, 160, 20, 10)
+rect(530, 160, 20, 10)
+rect(570, 160, 20, 10)
+rect(610, 160, 20, 10)
+
+rect(510, 170, 20, 10)
+rect(550, 170, 20, 10)
+rect(590, 170, 20, 10)
+rect(630, 170, 20, 10)
+
+rect(490, 180, 20, 10)
+rect(530, 180, 20, 10)
+rect(570, 180, 20, 10)
+rect(610, 180, 20, 10)
+
+rect(510, 190, 20, 10)
+rect(550, 190, 20, 10)
+rect(590, 190, 20, 10)
+rect(630, 190, 20, 10)
+
+rect(490, 200, 20, 10)
+rect(530, 200, 20, 10)
+rect(570, 200, 20, 10)
+rect(610, 200, 20, 10)
+
+rect(510, 210, 20, 10)
+rect(550, 210, 20, 10)
+rect(590, 210, 20, 10)
+rect(630, 210, 20, 10)
+
+rect(490, 220, 20, 10)
+rect(530, 220, 20, 10)
+rect(570, 220, 20, 10)
+rect(610, 220, 20, 10)
+
+rect(510, 230, 20, 10)
+rect(550, 230, 20, 10)
+rect(590, 230, 20, 10)
+rect(630, 230, 20, 10)
+
+rect(490, 240, 20, 10)
+rect(530, 240, 20, 10)
+rect(570, 240, 20, 10)
+rect(610, 240, 20, 10)
+
+rect(510, 250, 20, 10)
+rect(550, 250, 20, 10)
+rect(590, 250, 20, 10)
+rect(630, 250, 20, 10)
+
+rect(490, 260, 20, 10)
+rect(530, 260, 20, 10)
+rect(570, 260, 20, 10)
+rect(610, 260, 20, 10)
+
+rect(510, 270, 20, 10)
+rect(550, 270, 20, 10)
+rect(590, 270, 20, 10)
+rect(630, 270, 20, 10)
+
+rect(490, 280, 20, 10)
+rect(530, 280, 20, 10)
+rect(570, 280, 20, 10)
+rect(610, 280, 20, 10)
+
+rect(510, 290, 20, 10)
+rect(550, 290, 20, 10)
+rect(590, 290, 20, 10)
+rect(630, 290, 20, 10)
+
+rect(490, 300, 20, 10)
+rect(530, 300, 20, 10)
+rect(570, 300, 20, 10)
+rect(610, 300, 20, 10)
+
+rect(510, 310, 20, 10)
+rect(550, 310, 20, 10)
+rect(590, 310, 20, 10)
+rect(630, 310, 20, 10)
+
+
+	// set a fill color for queen sun
+  fill(255,255,0);
+	// draw a circle for queen sun
+	ellipse(115, 105, 165, 165);
+
+	// set a fill color for queen hair
+ fill(85, 46, 63);
+
+  //draw lips for queen
+  ellipse(114, 91, 115, 127)
+  ellipse(114, 30, 45, 35);
+  ellipse(114, 80, 85, 85);
+
+  //set a fill color for queen's dress
+  fill(223,7,73)
+
+  //draw a dress for queen
+  triangle(87, 190, 115, 105, 147, 190);
+
 	// set a fill color for queen sun
   fill(249, 244, 120);
 
+  // draw a circle for queen sun's neck
+  ellipse(115, 135, 25, 35);
+
+  //draw hands for the queen
+	ellipse(100, 170, 9, 18);
+	ellipse(130, 170, 9, 18);
+
+	// draw a ear for queen sun
+	ellipse(70, 100, 9, 18);
+	ellipse(160, 100, 9, 18);
+
+// fill color for queen's earing
+fill(255,0,78)
+//draw rings for the queen
+	ellipse(70, 110, 9, 9);
+	ellipse(160, 110, 9, 9);
+
+	// set a fill color for queen sun
+  fill(249, 244, 120);
 	// draw a circle for queen sun
 	ellipse(115, 100, 85, 85);
 
 	// set a fill color for queen sun's crown
 	fill(191, 185, 35);
 
-
   // draw a triangle for queen sun's crown
-  triangle(86, 70, 95, 40, 106, 70);
-  triangle(106, 70, 115, 40, 126, 70);
-  triangle(126, 70, 135, 40, 146, 70);
+  triangle(86, 68, 95, 35, 106, 68);
+  triangle(106, 68, 115, 25, 126, 68);
+  triangle(126, 68, 135, 35, 144, 68);
 
 
 	// set a fill color for queen sun's crown
@@ -75,11 +280,21 @@ stroke(51);
 
 	//draw nose for queen
 	arc(128, 105, 40, 60, PI, PI+QUARTER_PI);
-}
 
- // set a fill color for queen hair
-fill(85, 46, 63);
-//draw lips for queen
-ellipse(114, 121, 15, 7)
-ellipse(122, 121, 15, 7)
-ellipse(118, 125, 15, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
